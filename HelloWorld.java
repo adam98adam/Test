ed85ba791a9a653501451f7b040e73afc9f29565
Żebrowski Adam <Adam.Zebrowski@energa.pl>
2021-06-09 13:29:35 +0200

    Webhook test v7

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args) {
-        for(int i=0;i<80;i++) {
+        for(int i=0;i<30;i++) {
             System.out.println("Iteration number = " + i);
         }
     }
